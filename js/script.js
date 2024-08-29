5b42c265e71e74c1b0ae83b8a4ef3111840ec2dd
Nicolas Palena <139387673+pancreaso@users.noreply.github.com>
2024-08-29 18:32:22 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function displayMessage(){
+    compra = prompt("ingrese el monto a pagar: ")
+    if (compra == 250){
+        alert("compra exitosa")
+    }
+    else if(compra < 250){
+        alert("te falta plata crack")
+    }
+    else if(compra > 250){
+        alert("te pasaste de plata crack")
+    }
+}
+function displayMessage2(){
+    compra = prompt("ingrese el monto a pagar: ")
+    if (compra == 150){
+        alert("compra exitosa")
+    }
+    else if(compra < 150){
+        alert("te falta plata crack")
+    }
+    else if(compra > 150){
+        alert("te pasaste de plata crack")
+    }
+}
+
+function displayMessage3(){
+    compra = prompt("ingrese el monto a pagar: ")
+    if (compra == 200){
+        alert("compra exitosa")
+    }
+    else if(compra < 200){
+        alert("te falta plata crack")
+    }
+    else if(compra > 200){
+        alert("te pasaste de plata crack")
+    }
+}
